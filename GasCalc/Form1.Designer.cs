6db042763ca8ee5c29775725ecc1f4f1f58a5f2c
maeof <martynas.bagd@gmail.com>
2016-12-10 21:57:27 +0200

    Testing part
    Added references to DLLs of GMap;
    Testing the GMap;

--- GasCalc/Form1.Designer.cs
+++ GasCalc/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.MapTrip = new GMap.NET.WindowsForms.GMapControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MapTrip
+            // 
+            this.MapTrip.Bearing = 0F;
+            this.MapTrip.CanDragMap = true;
+            this.MapTrip.EmptyTileColor = System.Drawing.Color.Navy;
+            this.MapTrip.GrayScaleMode = false;
+            this.MapTrip.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.MapTrip.LevelsKeepInMemmory = 5;
+            this.MapTrip.Location = new System.Drawing.Point(12, 12);
+            this.MapTrip.MarkersEnabled = true;
+            this.MapTrip.MaxZoom = 18;
+            this.MapTrip.MinZoom = 1;
+            this.MapTrip.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.MapTrip.Name = "MapTrip";
+            this.MapTrip.NegativeMode = false;
+            this.MapTrip.PolygonsEnabled = true;
+            this.MapTrip.RetryLoadTile = 0;
+            this.MapTrip.RoutesEnabled = true;
+            this.MapTrip.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.MapTrip.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.MapTrip.ShowTileGridLines = false;
+            this.MapTrip.Size = new System.Drawing.Size(899, 329);
+            this.MapTrip.TabIndex = 0;
+            this.MapTrip.Zoom = 2D;
+            this.MapTrip.OnPositionChanged += new GMap.NET.PositionChanged(this.MapTrip_OnPositionChanged);
+            this.MapTrip.OnMapDrag += new GMap.NET.MapDrag(this.MapTrip_OnMapDrag);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(415, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(497, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 384);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MapTrip);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GMap.NET.WindowsForms.GMapControl MapTrip;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- GasCalc/Form1.cs
+++ GasCalc/Form1.cs
@@ -7,15 +7,146 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
 
 namespace GasCalc
 {
     public partial class Form1 : Form
     {
+        /*GMapOverlay markers;
+        GMapOverlay routes;
+        GMapOverlay objects;*/
+
+        readonly GMapOverlay top = new GMapOverlay();
+        internal readonly GMapOverlay objects = new GMapOverlay("objects");
+        internal readonly GMapOverlay routes = new GMapOverlay("routes");
+        internal readonly GMapOverlay polygons = new GMapOverlay("polygons");
+
+        bool first = true;
+        PointLatLng start;
+        PointLatLng end;
+
         public Form1()
         {
             InitializeComponent();
-            MessageBox.Show("Testing the version control.f");
+            //objects = new GMapOverlay("objects");
+            /*
+            markers = new GMapOverlay("markers");
+            GMapMarker marker = new GMarkerGoogle(
+                new PointLatLng(48.8617774, 2.349272),
+                GMarkerGoogleType.blue_pushpin);
+            markers.Markers.Add(marker);
+            MapTrip.Overlays.Add(markers);
+
+            routes = new GMapOverlay("routes");
+            GMapRoute route = new GMapRoute("test");
+            route.Points.Add(new PointLatLng(48.8617774, 2.349272));
+            route.Points.Add(new PointLatLng(54.6961334816182, 25.2985095977783));
+            MessageBox.Show(route.Distance.ToString());
+            routes.Routes.Add(route);
+            MapTrip.Overlays.Add(routes);*/
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            //MapTrip.MapProvider = LithuaniaMapProvider.Instance;
+            //GoogleMapProvider.Instance.ApiKey = "AIzaSyBbH9eC9C14-3CDRFJeagiVuhqkk7eSDi8";
+            //GoogleMapProvider.Instance.
+            MapTrip.MapProvider = BingMapProvider.Instance;
+                      
+            
+            //MapTrip.MapProvider = GMapProviders.OpenStreetMap;
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+            //MapTrip.SetPositionByKeywords("Lithuania");
+            MapTrip.SetPositionByKeywords("Lithuania");           
+
+            //MapTrip.OnMapDrag += MapTrip_OnMapDrag;
+            MapTrip.DoubleClick += MapTrip_DoubleClick;
+            MapTrip.MouseDoubleClick += MapTrip_MouseDoubleClick;
+
+
+            MapTrip.Overlays.Add(routes);
+            MapTrip.Overlays.Add(polygons);
+            MapTrip.Overlays.Add(objects);
+            MapTrip.Overlays.Add(top);
+
+        }
+
+        private void MapTrip_MouseDoubleClick(object sender, MouseEventArgs e)
+        {            
+            MessageBox.Show(MapTrip.FromLocalToLatLng(e.Location.X, e.Location.Y).ToString());
+            if (first)
+            {
+                start = MapTrip.FromLocalToLatLng(e.Location.X, e.Location.Y);
+                first = false;
+            }
+            else
+            {
+                end = MapTrip.FromLocalToLatLng(e.Location.X, e.Location.Y);
+                first = true;
+            }
+        }
+
+        private void MapTrip_DoubleClick(object sender, EventArgs e)
+        {
+            //MessageBox.Show(e.ToString());
+        }
+
+        private void MapTrip_OnMapDrag()
+        {
+            //MessageBox.Show("test");
+        }
+
+        private void MapTrip_OnPositionChanged(PointLatLng point)
+        {
+            //Point test = new Point();
+            //MessageBox.Show(point.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RoutingProvider rp = MapTrip.MapProvider as RoutingProvider;
+            if (rp == null)
+            {
+                rp = GMapProviders.OpenStreetMap; // use OpenStreetMap if provider does not implement routing
+            }
+
+            MapRoute route = rp.GetRoute(start, end, false, false, (int)MapTrip.Zoom);
+            if (route != null)
+            {
+                // add route
+                GMapRoute r = new GMapRoute(route.Points, route.Name);
+                r.IsHitTestVisible = true;
+                routes.Routes.Add(r);
+
+                // add route start/end marks
+                GMapMarker m1 = new GMarkerGoogle(start, GMarkerGoogleType.green_big_go);
+                m1.ToolTipText = "Start: " + route.Name;
+                m1.ToolTipMode = MarkerTooltipMode.Always;
+
+                GMapMarker m2 = new GMarkerGoogle(end, GMarkerGoogleType.red_big_stop);
+                m2.ToolTipText = "End: fetch from Google";
+                m2.ToolTipMode = MarkerTooltipMode.Always;
+
+                objects.Markers.Add(m1);
+                objects.Markers.Add(m2);
+
+                MapTrip.ZoomAndCenterRoute(r);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (GMapOverlay thisOverlay in MapTrip.Overlays)
+            {
+                foreach (GMapRoute thisRoute in thisOverlay.Routes)
+                {
+                    MessageBox.Show(thisRoute.Distance.ToString());
+                }
+            }
         }
     }
 }
